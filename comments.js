0d2cfacd4de01ebb5c2dec95358a03d9d8bfb7e2
Keshav Raj Chaudhary <keshavrajc31@gmail.com>
2025-03-07 17:09:13 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,65 @@
-// create web server to handle requests
-// 1. load modules
-var http = require('http');
+// create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var fs = require('fs');
-var url = require('url');
-var querystring = require('querystring');
-var comments = [];
-// 2. create server
-http.createServer(function (req, res) {
-    // parse the request
-    var url_parts = url.parse(req.url);
-    switch (url_parts.pathname) {
-        case '/':
-        case '/index':
-            display_form(url_parts, req, res);
-            break;
-        case '/add_comment':
-            add_comment(url_parts, req, res);
-            break;
-        case '/display_comments':
-            display_comments(url_parts, req, res);
-            break;
-        default:
-            display_404(url_parts, req, res);
-            break;
-    }
-}).listen(8080);
-console.log('Server running at http://
+
+// create a server
+var server = app.listen(3000, function(){
+	console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// set static directory
+app.use(express.static('public'));
+
+// use body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+// create a comment list
+var commentList = [];
+
+// create a comment list file
+var commentListFile = __dirname + '/public/commentList.json';
+
+// read comment list
+fs.readFile(commentListFile, 'utf8', function(err, data){
+	if (!err) {
+		commentList = JSON.parse(data);
+	}
+});
+
+// display comments
+app.get('/comments', function(req, res){
+	res.render('comments', {commentList: commentList});
+});
+
+// add comments
+app.post('/add', function(req, res){
+	var comment = req.body.comment;
+	if (comment) {
+		commentList.push(comment);
+		fs.writeFile(commentListFile, JSON.stringify(commentList), function(err){
+			if (err) {
+				throw err;
+			}
+		});
+	}
+	res.redirect('/comments');
+});
+
+// delete comments
+app.post('/delete', function(req, res){
+	var deleteNo = req.body.deleteNo;
+	if (deleteNo) {
+		commentList.splice(deleteNo, 1);
+		fs.writeFile(commentListFile, JSON.stringify(commentList), function(err){
+			if (err) {
+				throw err;
+			}
+		});
+	}
+	res.redirect('/comments');
+});
